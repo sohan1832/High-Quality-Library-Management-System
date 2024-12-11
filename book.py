f2da2bda34c6f51d2cb33c818ddf8d0928b7c866
Md. Sohanur Rahman Sohan <rafayedsohan2003@gmail.com>
2024-12-12 01:56:01 +0600

    Add files via upload

---
+++ book.py
@@ -0,0 +1,35 @@
+import add_books
+import view_all_books
+import restore_books_file
+from datetime import datetime
+import update_book_file, delete_book_file
+
+
+all_books = []
+
+
+while True:
+    print("Welcome to Library Management System")
+    print("0. Exit")
+    print("1. Add Books")
+    print("2. View All Books")
+    print("3. Book Update")
+    print("4. Book Remove/Delete")
+
+    all_books = restore_books_file.restore_all_books(all_books)
+    
+    menu = input("Select any number: ")
+    
+    if menu == "0":
+        print("Thanks for using Library Management System ")
+        break
+    elif menu == "1":
+        all_books = add_books.add_books(all_books)
+    elif menu == "2":
+        view_all_books.view_all_books(all_books)
+    elif menu == "3":
+        update_book_file.update_books(all_books)
+    elif menu == "4":
+        delete_book_file.delete_books(all_books)
+    else:
+        print("Choose a valid number")
